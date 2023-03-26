f03e2f362ba0e7e3c4a2f51b8789caa6298267f6
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-03-27 02:36:42 +0300

    Completed Lab

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
 
+    def initialize(title = "And Then There Were None", author = "Agatha Christie",page_count=272, genre = "Mystery")
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+    
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+      end
+    
+end
 
+booky = Book.new
+puts booky.title
+puts booky.author
+puts booky.page_count
+puts booky.genre
+puts booky.turn_page
